cbc8d2547131998ff68c5bc030fa28d9d6455304
fog66 <pol13112005@gmail.com>
2025-05-20 17:20:37 +0300

    add gitignore

--- build/CMakeFiles/image_processor.dir/src/image/color.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/image_processor.dir/src/image/color.o: \
-  /Users/polakovivan/Documents/Image_processor/src/image/color.cpp \
-  /Users/polakovivan/Documents/Image_processor/src/image/color.h
